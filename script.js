62b93bf1e2e390ce1e1e4b0fd7c4b997d29605bf
elimarc-afk <elimarccaballero251.@gmail.com>
2024-12-05 17:24:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+body {
+    font-family: 'Raleway', sans-serif;
+    margin: 0;
+    padding: 0;
+    color: #333;
+}
+
+.navbar {
+    background: gold;
+    color: green;
+    padding: 1rem;
+}
+
+.nav-links a {
+    color: green;
+}
